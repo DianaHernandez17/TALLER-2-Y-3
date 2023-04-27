bdead62528a383a8a39156c21cf41ac0e687fb99
Alejandra Hernandez <128438204+DianaHernandez17@users.noreply.github.com>
2023-04-26 19:39:58 -0500

    Add files via upload

---
+++ project_2687365/Ingresar.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE Ingresar;
+USE Ingresar;
+CREATE TABLE users_tbl(
+user_id int not null primary key auto_increment,
+user_firstname VARCHAR(40) not null,
+user_lastname VARCHAR(40) not null,
+user_email VARCHAR(60) not null,
+user_password VARCHAR(256)
+);
